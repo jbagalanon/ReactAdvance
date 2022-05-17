4abfa822629c478a33db52470014801c26c5eadd
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-05-18 01:34:11 +0800

    Initial Commit

---
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { data } from '../../../data';
+
+
+
+const UseStateArray = () => {
+  const [people, setPeople] = React.useState(data);
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>clear items</button>
+    </>
+  )
+};
+
+export default UseStateArray;
