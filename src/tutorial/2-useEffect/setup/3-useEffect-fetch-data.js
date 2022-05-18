9d93fa9d965cba7408fb7080def3de44f5ebee10
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-05-19 01:10:55 +0800

    fetching users from github  api  @3-useffect1

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -9,17 +9,34 @@ const UseEffectSecondArgument = () => {
 
     const response = await fetch(url);
     const users = await response.json();
+    setUsers(users)
     console.log(users);
-    //setUsers (users) = this code will run but it loops because of useeffect functions
+    // = this code will run but it loops because of useeffect functions. Solution is add dependency array in useEffect function.
   }
 
   //useeffect will run after every render and it cause looping for some codes
   useEffect(() => {
     getUsers();
-  })
+  }, []);
+
 
   return <>
     <h3>Github Users</h3>
+    <ul className="users">
+    {users.map((user) =>{
+      const {id, login, avatar_url, html_url} = user;
+
+      return (
+      <li key={id}>
+        <img src={avatar_url} alt={login} />
+        <div>
+          <h4>{login}</h4>
+          <a href={html_url}></a> 
+        </div>
+      </li>
+      );
+    })}
+    </ul>
 
 
   </>;
