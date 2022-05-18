e44ab888d8c2e74c5fc9276b2234980ea5a1031e
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-05-18 11:55:39 +0800

    changing specific value  @setup4

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -4,12 +4,16 @@ import React, { useState } from 'react';
 
 const UseStateObject = () => {
   const [person, setPerson] = useState({ name: 'Swordfish', age: 27, message: "Welcome to gulag" });
-  console.log(person);
+
+  const changeMessage = () => {
+    setPerson({...person, name:"Barracuda", message: "See you again"});
+  }
 
   return <>
   <h3>{person.name}</h3>
     <h3>{person.age}</h3>
     <h3>{person.message}</h3>
+    <button className="btn" onClick={changeMessage}>Change Message</button>
   </>;
 };
 
