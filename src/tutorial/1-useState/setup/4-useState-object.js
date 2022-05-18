b6e69f4250cd4070b1d1ef518ed8c23dfca42b36
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-05-18 11:48:22 +0800

    invoking the declared value  @setup4

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 
+
+
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({ name: 'Swordfish', age: 27, message: "Welcome to gulag" });
+  console.log(person);
+
+  return <>
+  <h3>{person.name}</h3>
+    <h3>{person.age}</h3>
+    <h3>{person.message}</h3>
+  </>;
 };
 
 export default UseStateObject;
