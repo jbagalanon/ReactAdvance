d32bf2494e0abffb5e58df3e438a1876b4f3d1cb
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-05-19 23:53:42 +0800

    short circuit

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 
-const url = 'https://api.github.com/users/jbagalanon';
+const url = 'https://api.github.com/users/jbagalanon3';
 
 
 //this is tutorial 20
@@ -14,37 +14,38 @@ const MultipleReturns = () => {
   useEffect(() => {
     fetch(url)
       .then((resp) => {
-        if (resp.status >= 200 && resp.status <= 299){
-      return resp.json();
-    }
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        }
         else {
-  setIsLoading(false);
-  setIsError(true);
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
 
-}
+        }
       })
       .then((user) => {
-  const { login } = user;
-  setUser(login);
-  setIsLoading(false);
-})
-  .catch((error) => console.log(error));
+        const { login } = user;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
 
   }, []);
 
-if (isLoading) {
-  return <h2>Loading...</h2>
+  if (isLoading) {
+    return <h2>Loading...</h2>
 
-}
+  }
 
-if (isError) {
-  return (
-    <h2>Error</h2>
-  );
-}
+  if (isError) {
+    return (
+      <h2>Error</h2>
+    );
+  }
 
 
-return <h1>{user} returns</h1>;
+  return <h1>{user} returns</h1>;
 };
 
 export default MultipleReturns;
