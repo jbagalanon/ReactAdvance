64b5dc73deb3cf6040e241c3b2ab6baf8fd30c67
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-05-25 06:13:46 +0800

    convert this multiple inputs into certain function

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -9,6 +9,7 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [age, setAge] = useState("");
   const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
@@ -46,6 +47,16 @@ const ControlledInputs = () => {
               id='email'
               name='email'
               value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='age'>Age : </label>
+            <input
+              type='text'
+              id='age'
+              name='age'
+              value={age}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
