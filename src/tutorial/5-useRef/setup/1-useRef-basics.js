93b678492c626518a2cfebbdfd3448515a95f4fc
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-05-28 06:48:56 +0800

    useref focus to value

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,23 +5,32 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
 
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+
   };
 
+  useEffect (() =>{
+    console.log (refContainer.current);
+   refContainer.current.focus();
+  });
 
   return (<>
     <form className="form" onSubmit={handleSubmit}>
       <div>
-        <input type="text" />
-        <button className="button">submmit</button>
+        <input type="text" ref={refContainer} />
+        <button className="submit">submit</button>
       </div>
 
     </form>
+    <div ref={divContainer}> hi </div>
 
   </>);
 };
