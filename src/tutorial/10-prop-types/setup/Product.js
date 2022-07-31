c4c12b6f1b3104aca63bbc81b6d88d785c3729a1
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-07-31 21:26:26 +0800

    implementing alternate   props done

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import defaultImage from '../../../assets/default-image.jpeg';
 
 const Product = ({ image, name, price }) => {
-  
-  
+  const url = image && image.url;
+
   return <article className='product'>
 
-    <img src={image.url} alt="" />
+    <img src={url || defaultImage} alt="" />
     <h4>{name}</h4>
     <p>$ {price || 3.99}</p>
 
