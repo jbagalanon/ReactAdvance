e5b949b72667f1c78a392b2fda4228924c5c8417
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-07-31 21:20:09 +0800

    alternative way implementing props

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = ({image, name, price}) => {
+const Product = ({ image, name, price }) => {
+  
+  
   return <article className='product'>
+
+    <img src={image.url} alt="" />
     <h4>{name}</h4>
-    <p>$ {price}</p>
+    <p>$ {price || 3.99}</p>
 
 
-
-    </article>;
+  </article>;
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+};
+
+//setting default props
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   image: defaultImage,
+
+// };
 
 export default Product;
